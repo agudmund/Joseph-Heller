d78b26b375499aa6f44cbaf2f7ade590f21a82ba
Aevar Gudmundsson <aevar.gudmundsson@gmail.com>
2017-04-16 18:18:03 +0200

    Making room for new friends by ejecting random ones when the limit hits

--- interact.py
+++ interact.py
@@ -13,22 +13,48 @@ from tweepy import API
 
 class StdOutListener(StreamListener):
 
-    def on_data(self, data):
+	def new_friends(self,name):
 
-        result = json.loads(data)
-        twitter.api.create_friendship(result['user']['screen_name'])
-        for n in [ n for n in result['entities']['user_mentions']]:
-        	if not n==[]:
-        		twitter.api.create_friendship(n['screen_name'])
+		twitter.api.destroy_friendship(id=choice(twitter.api.friends_ids()))
+		try:
+			twitter.api.create_friendship(name)
+			print 'Added %s' % name
+			return True
+		except tweepy.error.TweepError as e:
+			print e
+			return False
 
-        return True
+	def followings(self, name):
 
-    def on_error(self, status):
-        print status
+		try:
+			twitter.api.create_friendship(name)
+		except tweepy.error.TweepError as e:
+			while not self.new_friends(name):
+				print 'Current Follower Count: %s' % twitter.api.me().friends_count
+				self.new_friends(name)
+				sleep(randint(2,7))
+
+			# print 'This one, ' + str(choice(twitter.api.friends_ids()))
+			
+		return True
+
+	def on_data(self, data):
+
+		result = json.loads(data)
+		
+		self.followings(result['user']['screen_name'])
+		for n in [ n for n in result['entities']['user_mentions']]:
+			if not n==[]:
+				self.followings(n['screen_name'])
+
+		return True
+
+	def on_error(self, status):
+		print status
 
 class Twitterings:
 	def __init__(self):
-		self.name = 'Jospeh Heller'
+		self.name = 'Joseph Heller'
 		self.auth = OAuthHandler('Bn5Jnz1mf3JXBHAM7XvgbJMnH', 'WSbrPB3N5cIJaHM36LEiUaEywA8k6Zzi89hxoIekjr139pnXV1')
 		self.auth.set_access_token('853559235745107968-EXQDyL2bbbsAkof0xW5u2fg08rYwErE', 'fToyRaNywUAe5T4ShSGo6dnDLQfgnkETiE9fOfc2KwG6s' )
 		self.api = tweepy.API(self.auth)
